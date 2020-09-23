1adc966c48cd21a1b0068feed6025abf6242d254
Petrik Lajos Szg <info@petrik.hu>
2020-09-23 12:53:16 +0200

    2.  számok beolvassása majd az átlaguk kiszámolása

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,12 +10,24 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            int[] szamok = new int[10];
+          
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("add meg {0}. számot ", i+1);
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+            }
 
+            //számok átlaga
+            int ossz = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                ossz += szamok[i];
 
+            }
+            Console.WriteLine("A számok átlaga: {0}",ossz/szamok.Length);
 
-
-
-
+            Console.ReadLine();
         }
     }
 }
